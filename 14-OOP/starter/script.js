a564cd182ee2e32a398f8e94e5c192a8e656d31f
Victor <victormovileanu@gmail.com>
2022-07-16 23:13:34 +0200

    oop challenge 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+class Car {
+  constructor(name, speed) {
+    this.name = name
+    this.speed = speed
+  }
+  accelerate() {
+    this.speed += 10
+    console.log(this.speed)
+  }
+  break() {
+    this.speed -= 5
+    console.log(this.speed)
+  }
+}
+
+const car1 = new Car('BMW', 120)
+const car2 = new Car('Mercedes', 95)
+
+car1.accelerate()
+car1.accelerate()
+car1.accelerate()
+car2.break()
+car2.break()
+car2.break()
