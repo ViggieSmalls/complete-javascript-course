242edce4eefe6831b03ba9204fa6974cb9a4bb28
Victor <victormovileanu@gmail.com>
2022-08-05 09:01:49 +0200

    tabs

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -9,6 +9,12 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScroll = document.querySelector('.btn--scroll-to')
 const section1 = document.getElementById('section--1')
+const operationsTabContainer = document.querySelector('.operations__tab-container')
+const operationsTabs = document.querySelectorAll('.operations__tab')
+const operationsContents = document.querySelectorAll('.operations__content')
+
+////////////////////////////////////////////////////////////////////////////
+// Modal
 
 const openModal = function (evt) {
   evt.preventDefault()
@@ -31,4 +37,22 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+////////////////////////////////////////////////////////////////////////////
+// Smooth scroll
+
 btnScroll.addEventListener('click', () => {section1.scrollIntoView({behavior: "smooth"})})
+
+////////////////////////////////////////////////////////////////////////////
+// Tabs
+
+operationsTabContainer.addEventListener('click', function(evt) {
+  const btn = evt.target.closest('.btn')
+  if (!btn) return;
+  // remove all active classes
+  operationsTabs.forEach(el => el.classList.remove('operations__tab--active'))
+  operationsContents.forEach(el => el.classList.remove('operations__content--active'))
+  // activate clicked one
+  const id = btn.dataset.tab
+  btn.classList.add('operations__tab--active')
+  document.querySelector(`.operations__content--${id}`).classList.add('operations__content--active')
+})
