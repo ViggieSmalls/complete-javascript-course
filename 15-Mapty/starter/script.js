9621e79f940b640631ecf8e296091aeb64ca310f
Victor <victormovileanu@gmail.com>
2022-08-07 16:45:08 +0200

    render the form

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,35 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+const map = L.map('map')
+let mapClickEvt
+
+function mapOnClick(evt) {
+  form.classList.remove('hidden')
+  inputDistance.focus()
+  mapClickEvt = evt
+}
+
+function createMarker(msg="Workout!") {
+  const {lat, lng} = mapClickEvt.latlng
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      msg, {
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+      }
+    )
+    .openPopup();
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault()
+  createMarker()
+}
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -18,31 +47,18 @@ if (navigator.geolocation) {
       const latitude = position.coords.latitude
       const longitude = position.coords.longitude
       const coords = [latitude, longitude]
-
-      const map = L.map('map').setView(coords, 13);
+      map.setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
 
-      map.on('click', function (evt) {
-        const {lat, lng} = evt.latlng
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            "Workout!", {
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup'
-            }
-          )
-          .openPopup();
-      })
+      map.on('click', mapOnClick)
     },
     function () {
       alert('Can not get current location')
     }
   )
 }
+
+form.addEventListener('submit', handleFormSubmit)
