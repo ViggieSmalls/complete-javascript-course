997a1f5f76147f93e9f20808bab70b9824646b3c
Victor <victormovileanu@gmail.com>
2022-09-08 11:50:10 -0500

    use parcel to bundle app

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -1,4 +1,12 @@
 'use strict';
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
